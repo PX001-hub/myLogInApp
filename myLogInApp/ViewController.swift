10071014bb9416f7be80e0684c3d5d8380df8ccc
PX001-hub <54709568+PX001-hub@users.noreply.github.com>
2022-08-15 00:45:19 +0300

    +2

--- myLogInApp/ViewController.swift
+++ myLogInApp/ViewController.swift
@@ -9,11 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet var userNameTextField: UITextField!
+    
+    
+    @IBOutlet weak var forgotUserName: UIButton!
+    
+    
+    @IBOutlet weak var forgotPassword: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        segue.destination.navigationItem.title = userNameTextField.text
+    }
 
-
+    
+    @IBAction func forgotUserNameBtn(_ sender: UIButton) {
+    }
+    
+    @IBAction func forgotPasswordBtn(_ sender: UIButton) {
+    }
+    
+    
 }
 
